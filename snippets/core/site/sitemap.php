37ea266e256c3cd87c64ebe9825229eba2f50137
Jasper Schelling <jasper.schelling@gmail.com>
2022-08-01 21:17:33 +0200

    removed debug info

--- snippets/core/site/sitemap.php
+++ snippets/core/site/sitemap.php
@@ -2,7 +2,6 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach ($pages as $p): ?>
         <?php if (in_array($p->intendedTemplate(), $ignore)) continue ?>
-        <?= dump($p->intendedTemplate());?>
         <url>
             <loc><?= html($p->url()) ?></loc>
             <lastmod><?= $p->modified('c', 'date') ?></lastmod>
